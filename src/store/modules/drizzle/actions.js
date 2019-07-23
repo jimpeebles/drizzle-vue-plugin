933177403dc93f9280ae6c450abad15a06576b63
jimpeebles <james@concepttoproduct.us>
2019-07-23 18:06:14 -0400

    Add args fix

--- src/store/modules/drizzle/actions.js
+++ src/store/modules/drizzle/actions.js
@@ -13,10 +13,46 @@ export const REGISTER_CONTRACT = ({ commit, dispatch, rootState }, payload) => {
   }
 }
 
-const getCacheKey = (drizzleInstance, contractName, method, methodArgs) =>
-  drizzleInstance.contracts[contractName].methods[method].cacheCall(
-    ...methodArgs
+const getCacheKey = (drizzleInstance, contractName, method, methodArgs) => {
+  if (methodArgs.length > 0) {
+    const { utils } = drizzleInstance.web3
+    methodArgs = methodArgs.map(arg => {
+      console.log(arg, typeof arg)
+      return utils.toBN(arg)
+    })
+  }
+  console.log('methodArgs::', methodArgs)
+  console.group('getCacheKey')
+  console.log('di', drizzleInstance)
+  console.log('contractName', contractName)
+  console.log('method', method)
+  console.log('methodArgs', methodArgs)
+  const displayArgs = [...methodArgs].join(',')
+  console.log(
+    `drizzleInstance.contracts[${contractName}].methods[${method}].cacheCall(${displayArgs})`
   )
+  const { methods } = drizzleInstance.contracts[contractName]
+  console.log('methods...', Object.keys(methods))
+
+  console.groupEnd()
+  let cacheKey = '0x0'
+
+  try {
+    console.log('calling with methodArgs', methodArgs)
+    cacheKey = drizzleInstance.contracts[contractName].methods[
+      method
+    ].cacheCall(...methodArgs)
+  } catch (e) {
+    console.log('OOPS', e)
+    console.log('di', drizzleInstance)
+  }
+
+  console.log(
+    `cachekey:${contractName} - ${method} (${displayArgs}) = ${cacheKey}`
+  )
+
+  return cacheKey
+}
 
 // get cacheKey for all contracts/methods
 export const PROCESS_REGISTRATION_Q = ({
